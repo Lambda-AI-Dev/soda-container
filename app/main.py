2c0fa76d7c01ead8ada2552e082013e169a10300
Tenniel Miao <tennielmiao@gmail.com>
2020-04-09 14:44:04 -0400

    finish containerization

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import request
 import boto3
 
+import soda
+# pip install -r git+git://github.com/Lambda-AI-Dev/soda.git
+
 
 app = Flask(__name__)
 
@@ -13,3 +16,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
