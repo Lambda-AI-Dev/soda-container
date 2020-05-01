49724ee03171294f52cdc33245e9dca0573ce5ef
Tenniel Miao <tennielmiao@gmail.com>
2020-05-01 00:21:51 -0400

    Simple majority server with no gold data

--- app/main.py
+++ app/main.py
@@ -1,8 +1,69 @@
 from flask import Flask
 from flask import request
-import boto3
 
-import soda
+from dotenv import load_dotenv
+from pathlib import Path
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
+import os
+
+from soda.utils.graph import BipartiteGraph
+from soda.crowd.simpleMajorityClassifier import SimpleMajorityClassifier
+
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+REGION_NAME = os.getenv('REGION_NAME')
+
+session = boto3.Session(
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+    region_name=REGION_NAME
+)
+
+
+def get_task_ids(dataset_id):
+    dynamodb = session.resource('dynamodb')
+    task_table = dynamodb.Table(f"dataset_{dataset_id}")
+    return {x['taskId'] for x in task_table.scan()['Items']}
+
+
+def get_job_triplets(task_ids):
+    # task ids, labeler ids, results
+    U, V, E = [], [], []
+    dynamodb = session.resource('dynamodb')
+    task_table = dynamodb.Table("job")
+    scan = task_table.scan()
+    for job in scan["Items"]:
+        if job["taskId"] in task_ids:
+            U.append(job['taskId'])
+            V.append(job['labelerId'])
+            E.append(job['class'])
+    return U, V, E
+
+
+def encode_classes(E):
+    if not E:
+        return [], None
+    ret = []
+    classes = list(E[0].keys())
+    for e in E:
+        for i, c in enumerate(classes):
+            if e[c]:
+                ret.append(i)
+                break
+    return ret, classes
+
+
+def get_sparse_input(dataset_id):
+    U, V, E = get_job_triplets(get_task_ids(dataset_id))
+    E, classes = encode_classes(E)
+    return U, V, E, classes
+
+
 # pip install -r git+git://github.com/Lambda-AI-Dev/soda.git
 
 
@@ -12,6 +73,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/simple-majority/<dataset_id>/")
+def get_simple_majority(dataset_id):
+    U, V, E, classes = get_sparse_input(dataset_id)
+    content = request.json
+    n_classes = len(classes)
+    params = {}
+    # weight function specification to be continued
+    if "weight_func" in content:
+        params["weight_func"] = content["weight_func"]
+    smc = SimpleMajorityClassifier(n_classes=n_classes, **params)
+    bg = BipartiteGraph().add_edges_t(U, V, E)
+    return {u: classes[i] for u, i in zip(U, smc.predict_sparse(bg))}
 
 
 if __name__ == "__main__":
